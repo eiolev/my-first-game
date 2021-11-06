bc02ed71343afdcf8e3aebed83f2a745b75e6c18
KristenBoy1 <73790626+KristenBoy1@users.noreply.github.com>
2021-11-06 10:41:05 +0200

    xfy

---
+++ e/Assets/Scenes/PickUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PickUp : MonoBehaviour
+{
+    public int score;
+    public AudioSource audio;
+    public GameObject scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText.GetComponent<Text>().text = "Our Score"+ score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { score = 0;
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    { if (collision.gameObject.tag == "Coin")
+        {
+           
+
+            score++;
+            scoreText.GetComponent<Text>().text = "Our Score" + score;
+            audio.Play();
+            Destroy(collision.gameObject);
+        }   
+        
+    }
+}
---
+++ e/Assets/Scenes/helth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class helth : MonoBehaviour
+{
+    public int hp;
+    public GameObject myHealth;
+    public GameObject Emoty;
+    public GameObject winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = 10;
+        myHealth.GetComponent<Text>().text = "Health:" + hp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myHealth.GetComponent<Text>().text = "Health:" + hp;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "dangerblock")
+        {
+            hp--;
+
+                    
+            
+
+
+                
+        }
+
+
+        if (collision.gameObject.tag == "finish")
+        {
+            Emoty.SetActive(true);
+            winText.SetActive(true);
+        }
+
+    }
+    
+        
+}
